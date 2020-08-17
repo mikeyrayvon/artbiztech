bf6d81d402f3ec96e733c7254d3592370db40fda
mikeyrayvon <mikey@interglobal.vision>
2020-08-17 21:37:21 +0200

    add createTypes

--- gatsby-node.js
+++ gatsby-node.js
@@ -59,3 +59,44 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+exports.sourceNodes = ({ actions }) => {
+  const { createTypes } = actions
+  const typeDefs = `
+    type SiteSiteMetadata implements Node {
+      title: String
+      description: String
+    }
+    type MarkdownRemarkFrontmatter implements Node {
+      title: String
+      intro: Intro
+      about: [About]
+      team: Team
+      clients: [Clients]
+    }
+    type Intro {
+      heading: String
+      image: File
+    }
+    type About {
+      heading: String
+      text: String
+    }
+    type Team {
+      heading: String
+      description: String
+      members: [Members]
+    }
+    type Members {
+      name: String
+      title: String
+      image: File
+    }
+    type Clients {
+      company: String
+      logo: File
+    }
+
+  `
+  createTypes(typeDefs)
+}
